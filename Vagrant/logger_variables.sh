13203656a92a4147afe8f99d86cd6e6058be632b
syslogfather <46269408+syslogfather@users.noreply.github.com>
2021-10-04 14:29:21 -0400

    Update logger_variables.sh

--- Vagrant/logger_variables.sh
+++ Vagrant/logger_variables.sh
@@ -2,7 +2,7 @@
 
 # Get a free Maxmind license here: https://www.maxmind.com/en/geolite2/signup
 # Required for the ASNgen app to work: https://splunkbase.splunk.com/app/3531/
-export MAXMIND_LICENSE=""
+export MAXMIND_LICENSE="1M9lepCy2ONMRgDe"
 
 # Get a free Splunk Dev Trial License here: http://dev.splunk.com/page/developer_license_sign_up
 # To base64 encode on MacOS: cat Splunk.License | base64 | tr -d '\n' | pbcopy
