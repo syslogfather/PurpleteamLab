bba7e4b2f007905b592376479268857d21b156cc
syslogfather <46269408+syslogfather@users.noreply.github.com>
2021-10-04 15:23:32 -0400

    Update logger_bootstrap.sh

--- Vagrant/logger_bootstrap.sh
+++ Vagrant/logger_bootstrap.sh
@@ -4,7 +4,7 @@
 
 # Override existing DNS Settings using netplan, but don't do it for Terraform AWS builds
 if ! curl -s 169.254.169.254 --connect-timeout 2 >/dev/null; then
-  echo -e "    eth1:\n      dhcp4: true\n      nameservers:\n        addresses: [8.8.8.8,8.8.4.4]" >>/etc/netplan/01-netcfg.yaml
+  echo -e "    eth0:\n      dhcp4: true\n      nameservers:\n        addresses: [8.8.8.8,8.8.4.4]" >>/etc/netplan/01-netcfg.yaml
   netplan apply
 fi
 
@@ -80,10 +80,10 @@ test_prerequisites() {
   done
 }
 
-fix_eth1_static_ip() {
+fix_eth0_static_ip() {
   USING_KVM=$(sudo lsmod | grep kvm)
   if [ -n "$USING_KVM" ]; then
-    echo "[*] Using KVM, no need to fix DHCP for eth1 iface"
+    echo "[*] Using KVM, no need to fix DHCP for eth0 iface"
     return 0
   fi
   if [ -f /sys/class/net/eth2/address ]; then
@@ -92,28 +92,28 @@ fix_eth1_static_ip() {
       return 0
     fi
   fi
-  # There's a fun issue where dhclient keeps messing with eth1 despite the fact
-  # that eth1 has a static IP set. We workaround this by setting a static DHCP lease.
-  if ! grep 'interface "eth1"' /etc/dhcp/dhclient.conf; then
-    echo -e 'interface "eth1" {
+  # There's a fun issue where dhclient keeps messing with eth0 despite the fact
+  # that eth0 has a static IP set. We workaround this by setting a static DHCP lease.
+  if ! grep 'interface "eth0"' /etc/dhcp/dhclient.conf; then
+    echo -e 'interface "eth0" {
       send host-name = gethostname();
       send dhcp-requested-address 172.16.20.35;
     }' >>/etc/dhcp/dhclient.conf
     netplan apply
   fi
 
-  # Fix eth1 if the IP isn't set correctly
-  ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
-  if [ "$ETH1_IP" != "172.16.20.35" ]; then
+  # Fix eth0 if the IP isn't set correctly
+  ETH0_IP=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
+  if [ "$ETH0_IP" != "172.16.20.35" ]; then
     echo "Incorrect IP Address settings detected. Attempting to fix."
-    ifdown eth1
-    ip addr flush dev eth1
-    ifup eth1
-    ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
-    if [ "$ETH1_IP" == "172.16.20.35" ]; then
+    ifdown eth0
+    ip addr flush dev eth0
+    ifup eth0
+    ETH0_IP=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
+    if [ "$ETH0_IP" == "172.16.20.35" ]; then
       echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 172.16.20.35"
     else
-      echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
+      echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth0. Exiting because this will cause problems with other VMs."
       exit 1
     fi
   fi
@@ -268,7 +268,7 @@ main() {
   apt_install_prerequisites
   modify_motd
   test_prerequisites
-  fix_eth1_static_ip
+  fix_eth0_static_ip
   install_splunk
   postinstall_tasks
 }
